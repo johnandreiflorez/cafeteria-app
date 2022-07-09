dd2676b4026a407b83c6e7fd867685f3b6039ad9
John Andrei <johnandreiflorez@gmail.com>
2022-07-09 13:09:59 -0500

    Validator login

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
@@ -9,23 +10,35 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class InicioComponent implements OnInit {
 
-  user:UsuarioModel ={
+  user: UsuarioModel = {
     email: "",
     password: ""
   }
   @Output()
-  public inicio:EventEmitter<UsuarioModel>;
+  public inicio: EventEmitter<UsuarioModel>;
+  innerForm: FormGroup;
+  error: string;
   constructor(private authservice: AuthService,
-              private router:Router) {
-                
-      this.inicio = new EventEmitter<UsuarioModel>();
-   }
+    private router: Router) {
+    this.error = '';
+    this.innerForm =
+      new FormGroup({
+        Email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+        Password: new FormControl('', [Validators.required])
+      });
+
+    this.inicio = new EventEmitter<UsuarioModel>();
+  }
 
   ngOnInit(): void {
   }
 
-  logIn(){
+  logIn() {
+    if(this.innerForm.value.Password =='' || this.innerForm.value.Email ==''){
+      this.error = 'Ingrese todos los datos';
+      return;
+    }
     this.inicio.emit(this.user);
   }
-
 }
+
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,50 +11,46 @@ import { SesionService } from 'src/app/services/sesion.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  user:UsuarioModel ={}
+  user: UsuarioModel = {}
   constructor(private authservice: AuthService,
-              private router:Router,
-              private sesionService:SesionService) {
+    private router: Router,
+    private sesionService: SesionService) {
     ;
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  logIn(data:UsuarioModel){
+  logIn(data: UsuarioModel) {
     console.log(this.user);
     this.authservice.user.emit(this.user);
-    this.authservice.singin(data).subscribe((res:any) =>{
-       console.log(res);
-       if(res.id != -1){
-        this.sesionService.user= res;
+    this.authservice.singin(data).subscribe((res: any) => {
+      console.log(res);
+      if (res.id != -1) {
+        this.sesionService.user = res;
         this.sesionService.openSesion.emit(res);
-          switch(res?.rol?.id){
-            case 1:
-              //
-              this.router.navigate([`admin/${res.name}`]);
-              break;
-            case 2:
-              this.router.navigate([`vendedor/${res.name}`]);
-              break;
-            case 3:
-              this.router.navigate([`cliente/${res.name}`]);
-              break;
-            default:
-              
-              
-            
-         }
-       }
-       
-       //localStorage.setItem('token', res.token);
-       //
-       
+        switch (res?.rol?.id) {
+          case 1:
+            //
+            this.router.navigate([`admin/${res.name}`]);
+            break;
+          case 2:
+            this.router.navigate([`vendedor/${res.name}`]);
+            break;
+          case 3:
+            this.router.navigate([`cliente/${res.name}`]);
+            break;
+          default:
+
+
+
+        }
+      }
+
+      //localStorage.setItem('token', res.token);
+      //
+
     })
 
   }
-
-  
- 
-
 }
